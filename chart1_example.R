a91164f8be01d30a83e29dac1712d055656096bd
jykuang <jukuang@uw.edu>
2023-03-01 15:06:19 -0800

    Added my 3 data viz & edited Rmd file

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,34 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("ggplot2")
+library("tidyverse")
+library("dplyr")
+library("scales")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+spl_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv",
+                   stringsAsFactors = FALSE)
+
+spl_df <- spl_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
+
+
+usage_class_checkouts <- spl_df %>%
+  group_by(UsageClass, date) %>%
+  summarize(number_of_checkouts = sum(Checkouts, na.rm = TRUE))
+
+ggplot(usage_class_checkouts) +
+  geom_line(aes(
+    x = date,
+    y = number_of_checkouts,
+    color = UsageClass
+  )) +
+  geom_point(aes(
+    x = date,
+    y = number_of_checkouts,
+    color = UsageClass
+  )) +
+  scale_y_continuous(labels = label_number_si()) +
+  labs(
+    title = "SPL Physical vs. Digital Usage Checkouts (2022-23)",
+    x = "Month",
+    y = "Number of Checkouts",
+    color = "Item Usage Checkout"
+  )
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,37 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("ggplot2")
+library("tidyverse")
+library("dplyr")
+library("scales")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+spl_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv",
+                   stringsAsFactors = FALSE)
+
+spl_df <- spl_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01" ))
+spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
+
+
+collins_df <- spl_df %>%
+  filter(str_detect(Creator, "Suzanne")) %>% 
+  filter(str_detect(Creator, "Collins"))
+
+type_checkouts <- collins_df %>%
+  group_by(MaterialType, date) %>%
+  summarize(num_checkouts = sum(Checkouts, na.rm = TRUE))
+
+ggplot(type_checkouts) +
+  geom_line(aes(
+    x = date,
+    y = num_checkouts,
+    color = MaterialType
+  )) +
+  geom_point(aes(
+    x = date,
+    y = num_checkouts,
+    color = MaterialType
+  )) +
+  labs(
+    title = "Suzzane Collins SPL Checkouts from 2022-23", 
+    x = "Month", 
+    y = "Number of Checkouts",
+    color = "Material Type"
+  )
